d0a21e8361e7b517a989da27d682be7c82ba3191
sinliluo <luosenli127@pingan.com.cn>
2019-01-03 15:49:52 +0800

    webpack-dev-server

--- build/webpack/webpack.common.js
+++ build/webpack/webpack.common.js
@@ -1,12 +1,45 @@
 // var LodashModuleReplacementPlugin = require('loadsh-webpack-plugin');
 const path = require('path');
 const rootPath = path.resolve(__dirname, '../../');
-console.log(path.resolve(rootPath, './src/app.js'));
+console.log(path.resolve('./src/app.js'));
+console.log(path.resolve());
 module.exports = {
     mode: 'development',
     entry: path.resolve(rootPath, './src/app.js'),
     output: {
         path: rootPath,
         filename: 'bin/bundle/bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["es2015", "stage-0"],
+                        plugins: ["transform-runtime", "transform-es2015-arrow-functions"]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 1111,
+        open: true,
+        openPage: 'bin/'
+    },
+    resolve: {
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules'),
+        ],
+        alias: {
+            
+        }
+    },
+    plugins: [
+
+    ]
 };
