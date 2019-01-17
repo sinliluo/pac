0e8254542f811e4daab2252d854c47ae3ac5e0b6
sinliluo <luosenli127@pingan.com.cn>
2019-01-18 05:31:35 +0800

    delete log

--- build/cmd/prd.js
+++ build/cmd/prd.js
@@ -1,16 +1,8 @@
-console.log(0)
 const log = require('./com/log');
-console.log(01);
-
 const executeWebpack = require('./basic/executeWebpack');
-console.log(02);
-
 const prdConfig = require('../webpack/webpack.prd');
-console.log(1);
 
 module.exports = function () {
-    console.log(2);
-    
     return Promise.resolve()
         .then(() => log.start('prd'))
         .then(() => executeWebpack(prdConfig))
